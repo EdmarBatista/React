f354dc0f1467cf2670b62f878ff0f1830714d784
Edmar Batista <edmarbpaiva@gmail.com>
2024-08-02 07:01:35 -0300

    02_hooks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import HookState01 from "./components/UseState_01"
+
+
+// Como é uma função anónima ela só vai receber o nome quando eu usar o import
+export default () => {
+
+    return (
+        <>
+            <h1>REACT HOOKS</h1>
+            <HookState01 />
+          
+        </>
+    )
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom/client' // Dois imports importantes do react
+
+import App from './App';  // O nome do componente só veio agora, pois no App ela está como anónima
+
+
+ReactDOM.createRoot(document.querySelector('#root')).render( 
+    <div>
+        <App />
+   
+    
+  
+
+    </div>
+)
